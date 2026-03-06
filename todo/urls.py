59d27cfca3a81f025726120b1fc5b4e3ad680ff1
han jihye <hanjihye2712@gmail.com>
2026-03-06 11:07:53 +0900

    test 2번 주석처리

--- todo/urls.py
+++ todo/urls.py
@@ -1,27 +1,35 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-
-from .views.api_views import (
-    TodoViewSet,
-    TodoListAPI,
-    TodoCreateAPI,
-    TodoRetrieveAPI,
-    TodoUpdateAPI,
-    TodoDeleteAPI,
+from .views.templates_views import (
+    TodoListView,
+    TodoCreateView,
+    TodoDetailView,
+    TodoUpdateView,
 )
+
+# from .views.api_views import TodoListAPI, TodoCreateAPI, TodoRetrieveAPI, TodoUpdateAPI, TodoDeleteAPI, TodoViewSet
+from .views.api_views import TodoViewSet
+
+from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register("view", TodoViewSet, basename="todo")
 
+
 app_name = "todo"
 
 urlpatterns = [
-    # ✅ APIView 기반 (tests_crud.py가 치는 주소)
-    path("api/list/", TodoListAPI.as_view(), name="todo_api_list"),
-    path("api/create/", TodoCreateAPI.as_view(), name="todo_api_create"),
-    path("api/retrieve/<int:pk>/", TodoRetrieveAPI.as_view(), name="todo_api_retrieve"),
-    path("api/update/<int:pk>/", TodoUpdateAPI.as_view(), name="todo_api_update"),
-    path("api/delete/<int:pk>/", TodoDeleteAPI.as_view(), name="todo_api_delete"),
-    # ✅ ViewSet 기반 (testsviewset*가 치는 주소)
+    # path("list/", views.todo_list, name="todo_List"), # 첫 테스트용
+    path("list/", TodoListView.as_view(), name="list"),
+    path("create/", TodoCreateView.as_view(), name="todo_create"),
+    path("detail/<int:pk>/", TodoDetailView.as_view(), name="todo_Detail"),
+    path("update/<int:pk>/", TodoUpdateView.as_view(), name="todo_Update"),
+    # api
+    # path("api/list/", TodoListAPI.as_view(), name="todo_api_list"),
+    # path("api/create/", TodoCreateAPI.as_view(), name="todo_api_create"),
+    # path("api/retrieve/<int:pk>/", TodoRetrieveAPI.as_view(), name="todo_api_retrieve"),
+    # path("api/update/<int:pk>/", TodoUpdateAPI.as_view(), name="todo_api_update"),
+    # path("api/delete/<int:pk>/", TodoDeleteAPI.as_view(), name="todo_api_delete"),
+    # Viewsets CRUD를 하나로 통일
     path("viewsets/", include(router.urls)),
+    # 127.0.0.1:8000/todo/viewsets/view/
 ]
